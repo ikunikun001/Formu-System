9b8943c011f6d0f690e918575dcd7580a64e1349
root <2651243828@qq.com>
2024-07-19 22:14:48 +0800

    阿里云对象存储OSS工具

---
+++ src/main/java/com/wjf/forumsystem/utils/AliyunOSSUtils.java
@@ -0,0 +1,63 @@
+package com.wjf.forumsystem.utils;
+
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
+import com.aliyun.oss.model.PutObjectRequest;
+import com.aliyun.oss.model.PutObjectResult;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class AliyunOSSUtils {
+    // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+    private static final String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
+    // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
+    private static final String ACCESS_KEY_ID="LTAI5tLSNtVVFnvFCZNBX9w7";
+    private static final String ACCESS_KEY_SECRET="QtywfijWiUhJBWcsdqXpfFEfnWiyif";
+    //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+    // 填写Bucket名称，例如examplebucket。
+    private static final String BUCKENAME = "formu-system";
+    public static String upload(String objectName, InputStream in) throws Exception {
+
+        String url="";
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(ENDPOINT, ACCESS_KEY_ID,ACCESS_KEY_SECRET);
+
+        try {
+            // 填写字符串。
+            String content = "Hello OSS，你好世界";
+
+            // 创建PutObjectRequest对象。
+            PutObjectRequest putObjectRequest = new PutObjectRequest(BUCKENAME, objectName, in);
+
+            // 如果需要上传时设置存储类型和访问权限，请参考以下示例代码。
+            // ObjectMetadata metadata = new ObjectMetadata();
+            // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+            // metadata.setObjectAcl(CannedAccessControlList.Private);
+            // putObjectRequest.setMetadata(metadata);
+
+            // 上传字符串。
+            PutObjectResult result = ossClient.putObject(putObjectRequest);
+            url="https://"+BUCKENAME+"."+ENDPOINT.substring(ENDPOINT.lastIndexOf("/")+1)+"/"+objectName;
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+        return url;
+    }
+}
