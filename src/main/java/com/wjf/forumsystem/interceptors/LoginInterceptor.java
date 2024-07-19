46be9cb6cbed1cddf5de729caeb3e97cd93b83a1
root <2651243828@qq.com>
2024-07-19 22:19:55 +0800

    对比存在Redis的令牌，不对就拦截

--- src/main/java/com/wjf/forumsystem/interceptors/LoginInterceptor.java
+++ src/main/java/com/wjf/forumsystem/interceptors/LoginInterceptor.java
@@ -4,6 +4,9 @@ import com.wjf.forumsystem.utils.JwtUtils;
 import com.wjf.forumsystem.utils.ThreadLocalUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,10 +14,17 @@ import java.util.Map;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token=request.getHeader("Authorization");
         try {
+            ValueOperations<String, String> op = redisTemplate.opsForValue();
+            String redisToken=op.get(token);
+            if(redisToken==null){
+                throw new RuntimeException();
+            }
             Map<String,Object> cliams=JwtUtils.ParseToken(token);
             ThreadLocalUtils.set(cliams);
             return true;
