4ec0df1f636de0b389ec0e585b41b7810485ca29
吴建飞 <2651243828@qq.com>
2024-10-31 15:06:29 +0800

    注册登录拦截器

--- src/main/java/com/wjf/forumsystem/config/WebConfig.java
+++ src/main/java/com/wjf/forumsystem/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.wjf.forumsystem.config;
 
+import com.github.pagehelper.PageInterceptor;
 import com.wjf.forumsystem.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Properties;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Autowired
@@ -13,4 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginInterceptor).excludePathPatterns("/user/login","/user/register");
     }
-}
+    @Bean
+    public PageInterceptor pageInterceptor() {
+        PageInterceptor pageInterceptor = new PageInterceptor();
+        Properties properties = new Properties();
+        // 分页合理化，true开启，如果分页参数不合理会自动修正。默认false不启用
+        properties.setProperty("reasonable", "true");
+        pageInterceptor.setProperties(properties);
+        return pageInterceptor;
+    }
+
+
+    }
